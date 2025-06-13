e10a15048c5fb03c5efe59592a1ba9a7f823c488
unknown <molinabertulfo26@gmail.com>
2025-06-12 22:20:33 -0500

    esta carpeta contiene actividad1.py

---
+++ actividad2.py
@@ -0,0 +1,8 @@
+# definimos variables de diferentes tipos 
+
+nombre= "carlos"
+edad=28
+#usamos un formato de cadena para insertar variables en una cadena de texto
+mensaje=f"hola,mi nimbre es {nombre} y tengo {edad} años."
+#ahora imprimimos el mensaje formateado 
+print(mensaje)
